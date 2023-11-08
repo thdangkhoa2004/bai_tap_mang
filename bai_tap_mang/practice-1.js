b8df56fd87e85b456907bff99d85094e66ccd9fa
thdangkhoa2004 <khoab2204786@student.ctu.edu.vn>
2023-11-08 16:34:14 +0700

    add bai_tap_mang file

---
+++ bai_tap_mang/practice-1.js
@@ -0,0 +1,4 @@
+// Bài 1: Hãy viết một chương trình để đảo ngược các phần tử trong list(array)
+let aList = [100, 200, 300, 400, 500];
+console.log(aList.reverse());
+// Output: [500, 400, 300, 200, 100]
